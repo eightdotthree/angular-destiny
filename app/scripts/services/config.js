65f15daccf00abb4e35b8846b617fe5f94dc4365
Ryan Sprake <ryan.sprake@gmail.com>
2015-12-12 11:40:13 -0500

    Added an app config file.

---
+++ app/scripts/services/config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name destinyApp.config
+ * @description
+ * # config
+ * Constant in the destinyApp.
+ */
+angular.module('destinyApp')
+  .constant('CONFIG', {
+
+    /**
+     * @ngdoc property
+     * @name directiveTemplateBaseUrl
+     * @description
+     * The base url path for directive templates.
+     */
+    directiveTemplateBaseUrl: 'scripts/directives/templates/'
+
+  });
